6642685114078692613d1c6ced37d4b0464e6d17
jhonata conceição <jhonata.silva52@etec.sp.gov.br>
2025-06-01 22:43:48 -0300

    37

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,7 +16,7 @@
     <ul class="nav-list">
         <img src="assets/images/logo.png" alt="logotipo de um carro" class="logo">
         <li><a href="index.php" class="letter-list">Home</a></li>
-        <li><a href="grupo.php" class="letter-list">Grupos</a></li>
+        <li><a href="pag_grupo.php" class="letter-list">Grupos</a></li>
         <li><a href="#" class="letter-list">Vendas</a></li>
         <li><a href="#" class="letter-list">Sobre</a></li>
         <li><a href="banco/logout.php" class="letter-list">Logout</a></li>
