9678434e71d056b5e31e2c0bb378da5028bcec4b
dwtazer <dwtazer@gmail.com>
2025-06-03 22:04:11 -0300

    v39

--- pag_grupo.php
+++ pag_grupo.php
@@ -16,10 +16,10 @@ if (session_status() == PHP_SESSION_NONE) {
 $meetcar = new MeetCarFunctions();
 
 
-$loggedInUserId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null; 
+$userId = $_SESSION['user_id']; 
 
 // Chama a função buscarGrupos passando o ID do usuário logado
-$grupos = $meetcar->buscarGrupos($loggedInUserId);
+$grupos = $meetcar->buscarGrupos($userId);
 
 ?>
 
@@ -39,42 +39,44 @@ $grupos = $meetcar->buscarGrupos($loggedInUserId);
             <h1>Comunidade de Grupos</h1>
             <?php if (!empty($grupos)) { ?>
                 <?php foreach ($grupos as $grupo) { ?>
-                    <div class="grupo">
-                        <div class="g-fotinha">
-                            <?php if (!empty($grupo['img_grupo'])) { ?>
-                                <img src="assets/images/groups/<?php echo htmlspecialchars($grupo['img_grupo']); ?>" alt="Imagem do Grupo">
-                            <?php } ?>
-                        </div>
-                        <div class="g-info">
-                            <div class="g-nome"><?php echo htmlspecialchars($grupo['nome_grupo']); ?></div>
-                            <div class="g-desc">
-                                <?php
-                                    $descricao = htmlspecialchars($grupo['descricao_grupo']);
-                                if (strlen($descricao) > 150) {
-                                    echo substr($descricao, 0, 150) . '...';
-                                } else {
-                                    echo $descricao;
-                                }
-                                ?>
-                            </div>
-                            <?php if (!empty($grupo['nome_temas'])) { ?>
-                                <span class="p-tag" style="background-color: <?php echo htmlspecialchars($grupo['cor_fundo']); ?>; color: <?php echo ($grupo['cor_letras'] == 1) ? '#FFFFFF' : '#000000';  ?>">
-                                    <?php echo htmlspecialchars($grupo['nome_temas']); ?>
-                                </span>
-                            <?php } ?>
-                            <div class="g-letrinhas">
-                                Membros: <?php echo $grupo['membros_count']; ?>
-                                <?php if ($grupo['user_participando']) { ?>
-                                    <span style="font-weight: bold; color: #28a745; margin-left: 5px;">
-                                        (Você participa)
-                                    </span>
+                    <a href="./grupo.php?id=<?= $grupo['id_grupo'] ?>">
+                        <div class="grupo">
+                            <div class="g-fotinha">
+                                <?php if (!empty($grupo['img_grupo'])) { ?>
+                                    <img src="assets/images/groups/<?php echo htmlspecialchars($grupo['img_grupo']); ?>" alt="Imagem do Grupo">
                                 <?php } ?>
                             </div>
-                            <div class="g-letrinhas">
-                                Criado por: <?php echo htmlspecialchars($grupo['nome_user'] . ' ' . $grupo['sobrenome_user']); ?>
+                            <div class="g-info">
+                                <div class="g-nome"><?php echo htmlspecialchars($grupo['nome_grupo']); ?></div>
+                                <?php if (!empty($grupo['nome_temas'])) { ?>
+                                    <span class="p-tag" style="background-color: <?php echo htmlspecialchars($grupo['cor_fundo']); ?>; color: <?php echo ($grupo['cor_letras'] == 1) ? '#FFFFFF' : '#000000';  ?>">
+                                        <?php echo htmlspecialchars($grupo['nome_temas']); ?>
+                                    </span>
+                                <?php } ?>
+                                <div class="g-desc">
+                                    <?php
+                                        $descricao = htmlspecialchars($grupo['descricao_grupo']);
+                                    if (strlen($descricao) > 150) {
+                                        echo substr($descricao, 0, 150) . '...';
+                                    } else {
+                                        echo $descricao;
+                                    }
+                                    ?>
+                                </div>
+                                <div class="g-letrinhas">
+                                    Membros: <?php echo $grupo['membros_count']; ?>
+                                    <?php if ($grupo['user_participando']) { ?>
+                                        <span style="font-weight: bold; color: #28a745; margin-left: 5px;">
+                                            (Você participa)
+                                        </span>
+                                    <?php } ?>
+                                </div>
+                                <div class="g-letrinhas">
+                                    Criado por: <?php echo htmlspecialchars($grupo['nome_user'] . ' ' . $grupo['sobrenome_user']); ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 <?php } ?>
             <?php } else { ?>
                 <p>Nenhum grupo foi encontrado. Seja o primeiro a criar um!</p>
