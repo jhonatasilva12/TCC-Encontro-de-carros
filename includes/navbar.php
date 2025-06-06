eac6c62f01ab45f3e0230f060d30ef1d4d2481f2
dwtazer <dwtazer@gmail.com>
2025-06-06 20:04:56 -0300

    v43

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,21 +5,19 @@
 <header class="header">
     <nav class="nav">
 
-    <div class="nav-list">
+    <div id="icones" class="nav-list">
         <a href="index.php" class="icon-list"><i class="fas fa-home"></i></a>
-        <a href="#" class="icon-list"><i class="fas fa-users"></i></a>
+        <a href="#pag_grupo" class="icon-list"><i class="fas fa-users"></i></a>
+        <a href="calendar.php" class="icon-list"><i class="fas fa-calendar-days"></i></a>
         <a href="#" class="icon-list"><i class="fas fa-shopping-cart"></i></a>
-        <a href="#" class="icon-list"><i class="fas fa-info-circle"></i></a>
-        <a href="banco/logout.php" class="icon-list"><i class="fas fa-sign-out-alt"></i></a>
     </div>
         
-    <ul class="nav-list">
+    <ul id="letras" class="nav-list">
         <img src="assets/images/logo.png" alt="logotipo de um carro" class="logo">
         <li><a href="index.php" class="letter-list">Home</a></li>
         <li><a href="pag_grupo.php" class="letter-list">Grupos</a></li>
+        <li><a href="calendar.php" class="letter-list">calendario</a></li>
         <li><a href="#" class="letter-list">Vendas</a></li>
-        <li><a href="#" class="letter-list">Sobre</a></li>
-        <li><a href="banco/logout.php" class="letter-list">Logout</a></li>
     </ul>
 
       </nav>
