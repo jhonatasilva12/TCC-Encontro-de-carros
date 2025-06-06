7e0c1651cfcfe999415c04a520d3685ada8b691c
dwtazer <dwtazer@gmail.com>
2025-06-05 22:14:40 -0300

    v42

--- includes/user_box.php
+++ includes/user_box.php
@@ -5,19 +5,29 @@ $userId = $_SESSION['user_id'] ?? null;
 
 $user = $meetcar->buscarUserPorId($userId);
 ?>
+
+<div class="tabs" style="display: none;">
+    <div id="box-1" class="tab ativo" data-tab="1"><i class="fas fa-image"></i>Posts</div>
+    <div id="box-2" class="tab" data-tab="2"><i class="fas fa-calendar-days"></i>Eventos</div>
+    <div id="box-3" class="tab" data-tab="3"><i class="fas fa-users"></i>Grupos</div>
+    <div id="box-4" class="tab" data-tab="4"><i class="fas fa-user"></i>Usuários</div>
+</div>
+
 <div class="user-box" data-id="<?= $user['id_user'] ?>">
+
     <div class="box-topo">
-        <img src="assets/images/users/user_padrao.jpg" alt="imagem do user logado">
+        <img src="assets/images/users/<?php echo ($post['img_user']); ?>" alt="imagem do user logado">
         <label><?php echo htmlspecialchars($user['nome_user']) . ' ' . htmlspecialchars($user['sobrenome_user']); ?></label>
     </div>
-
-    <div class="box-centro">
-        <p><?php echo htmlspecialchars($user['bio_user']); ?></p>
-    </div>
+    <?php if (!empty($user['bio_user'])): ?>
+        <div class="box-centro">
+            <p><?php echo htmlspecialchars($user['bio_user']); ?></p>
+        </div>
+    <?php endif; ?>
 
     <div class="box-inferior">
-    <a href="user.php?id=<?= $user['id_user'] ?>" class="letter-list"><i class="fas fa-user-alt">ver mais</i></a>
-    <a href="banco/logout.php" class="letter-list"><i class="fas fa-sign-out-alt">Logout</i></a>
+        <a href="user.php?id=<?= $user['id_user'] ?>"><i class="fas fa-user-alt">ver mais</i></a>
+        <a href="banco/logout.php"><i class="fas fa-sign-out-alt">Logout</i></a>
     </div>
     
 </div>
