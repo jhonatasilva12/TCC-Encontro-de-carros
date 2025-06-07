563fca5d82afddf1bc62ccd322a301fd435af2ad
dwtazer <dwtazer@gmail.com>
2025-06-06 21:19:48 -0300

    v43

--- includes/feed.php
+++ includes/feed.php
@@ -32,8 +32,10 @@ usort($conteudos, function($a, $b) {
     <?php
         foreach ($conteudos as $item) {
             if ($item['tipo'] === 'post') {
+                $post = $item['dados'];
                 include('includes/posts.php');
             } else {
+                $evento = $item['dados'];
                 include('includes/eventos.php');
             }
         }
