f40a71b062a155cfbe5ad5d7175e623a86e56592
dwtazer <dwtazer@gmail.com>
2025-06-07 18:44:16 -0300

    v45

--- banco/db_meetcar.sql
+++ banco/db_meetcar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03/06/2025 às 20:59
+-- Tempo de geração: 07/06/2025 às 23:43
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -114,15 +114,16 @@ CREATE TABLE `tb_evento` (
   `numero_evento` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `cidade_evento` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `estado_evento` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `fk_id_criador` int(11) NOT NULL
+  `fk_id_criador` int(11) NOT NULL,
+  `fk_id_grupo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Despejando dados para a tabela `tb_evento`
 --
 
-INSERT INTO `tb_evento` (`id_evento`, `nome_evento`, `img_evento`, `descricao_evento`, `data_post`, `data_inicio_evento`, `data_termino_evento`, `valor_pedestre`, `valor_exposicao`, `rua_evento`, `numero_evento`, `cidade_evento`, `estado_evento`, `fk_id_criador`) VALUES
-(8, 'FTG Meeting', 'event_683cc321ac754.jpeg', 'Prepare-se para um encontro épico! O FTG Meeting reúne os amantes de automobilismo para um espetáculo de máquinas incríveis, pilotos talentosos e uma atmosfera vibrante cheia de adrenalina.', '2025-06-01 21:16:17', '2025-05-22 20:00:00', '2025-05-23 00:00:00', '15,00', '50,00', 'Av. Cruzeiro do Sul', '1000', 'são paulo', 'SP', 6);
+INSERT INTO `tb_evento` (`id_evento`, `nome_evento`, `img_evento`, `descricao_evento`, `data_post`, `data_inicio_evento`, `data_termino_evento`, `valor_pedestre`, `valor_exposicao`, `rua_evento`, `numero_evento`, `cidade_evento`, `estado_evento`, `fk_id_criador`, `fk_id_grupo`) VALUES
+(8, 'FTG Meeting', 'event_683cc321ac754.jpeg', 'Prepare-se para um encontro épico! O FTG Meeting reúne os amantes de automobilismo para um espetáculo de máquinas incríveis, pilotos talentosos e uma atmosfera vibrante cheia de adrenalina.', '2025-06-01 21:16:17', '2025-05-22 20:00:00', '2025-05-23 00:00:00', '15,00', '50,00', 'Av. Cruzeiro do Sul', '1000', 'são paulo', 'SP', 6, NULL);
 
 -- --------------------------------------------------------
 
@@ -159,7 +160,8 @@ CREATE TABLE `tb_post` (
   `texto_post` varchar(500) NOT NULL,
   `data_post` timestamp NOT NULL DEFAULT current_timestamp(),
   `fk_id_user` int(11) NOT NULL,
-  `fk_id_tipo_post` int(11) NOT NULL
+  `fk_id_tipo_post` int(11) NOT NULL,
+  `fk_id_grupo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -209,8 +211,8 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `nome_user`, `sobrenome_user`, `bio_user`, `img_user`, `data_nasc_user`, `telefone_user`, `cpf_user`, `email_user`, `senha_user`) VALUES
-(6, 'david', 'de jesus almeida', '', 'user_padrao.jpg', NULL, '(11) 22222-2222', NULL, 'dwtazer@gmail.com', '$2y$10$.b4Nc9dgLuUpO7vfWXSai.aG2pJePLfIbIj8wio9.jqM/.YfRvUi6'),
-(9, 'tester', 'padrao', '', 'user_padrao.jpg', NULL, '(99) 99999-9999', NULL, 'tester@gmail.com', '$2y$10$I./0s6S9mLoykNOJ38wzLumKkXFidIisjAoq2eqyuIEv5lFfE12o.');
+(6, 'david', 'de jesus almeida', 'talvez o único que utilizou Pokémon para fazer os testes de post, evento e até grupos no site. Quem sabe? eu mesmo não sei kkkkkkkkkkkk', 'user_david.png', NULL, '(11) 22222-2222', NULL, 'dwtazer@gmail.com', '$2y$10$.b4Nc9dgLuUpO7vfWXSai.aG2pJePLfIbIj8wio9.jqM/.YfRvUi6'),
+(9, 'tester', 'padrão', '', 'user_padrao.jpg', NULL, '(99) 99999-9999', NULL, 'tester@gmail.com', '$2y$10$I./0s6S9mLoykNOJ38wzLumKkXFidIisjAoq2eqyuIEv5lFfE12o.');
 
 -- --------------------------------------------------------
 
@@ -231,7 +233,8 @@ CREATE TABLE `temas_grupo` (
 --
 
 INSERT INTO `temas_grupo` (`id_temas_grupo`, `nome_temas`, `descricao_temas`, `cor_fundo`, `cor_letras`) VALUES
-(6, 'random', '', '#000000', 1);
+(6, 'random', '', '#000000', 1),
+(7, 'carros', '', '#990000', 1);
 
 -- --------------------------------------------------------
 
@@ -296,7 +299,8 @@ ALTER TABLE `tb_comentario`
 --
 ALTER TABLE `tb_evento`
   ADD PRIMARY KEY (`id_evento`),
-  ADD KEY `fk_id_criador` (`fk_id_criador`);
+  ADD KEY `fk_id_criador` (`fk_id_criador`),
+  ADD KEY `fk_id_grupo` (`fk_id_grupo`);
 
 --
 -- Índices de tabela `tb_grupo`
@@ -311,7 +315,8 @@ ALTER TABLE `tb_grupo`
 ALTER TABLE `tb_post`
   ADD PRIMARY KEY (`id_post`),
   ADD KEY `fk_id_user` (`fk_id_user`),
-  ADD KEY `fk_id_tipo_post` (`fk_id_tipo_post`);
+  ADD KEY `fk_id_tipo_post` (`fk_id_tipo_post`),
+  ADD KEY `fk_id_grupo` (`fk_id_grupo`);
 
 --
 -- Índices de tabela `tb_tipo_post`
@@ -364,7 +369,7 @@ ALTER TABLE `tb_grupo`
 -- AUTO_INCREMENT de tabela `tb_post`
 --
 ALTER TABLE `tb_post`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de tabela `tb_tipo_post`
@@ -382,7 +387,7 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT de tabela `temas_grupo`
 --
 ALTER TABLE `temas_grupo`
-  MODIFY `id_temas_grupo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_temas_grupo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restrições para tabelas despejadas
@@ -427,7 +432,14 @@ ALTER TABLE `tb_comentario`
 -- Restrições para tabelas `tb_evento`
 --
 ALTER TABLE `tb_evento`
-  ADD CONSTRAINT `tb_evento_ibfk_1` FOREIGN KEY (`fk_id_criador`) REFERENCES `tb_user` (`id_user`);
+  ADD CONSTRAINT `tb_evento_ibfk_1` FOREIGN KEY (`fk_id_criador`) REFERENCES `tb_user` (`id_user`),
+  ADD CONSTRAINT `tb_evento_ibfk_2` FOREIGN KEY (`fk_id_grupo`) REFERENCES `tb_grupo` (`id_grupo`);
+
+--
+-- Restrições para tabelas `tb_post`
+--
+ALTER TABLE `tb_post`
+  ADD CONSTRAINT `tb_post_ibfk_1` FOREIGN KEY (`fk_id_grupo`) REFERENCES `tb_grupo` (`id_grupo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
